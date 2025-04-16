13d1107105aa4073cf8e679f3ea50f409a7ec7f8
Dean Prach <deanprach@gmail.com>
2025-04-16 10:27:37 -0700

    get state total quotes from  api

---
+++ src/Hooks/useStateTotals.js
@@ -0,0 +1,26 @@
+import { useEffect,useState,useCallback } from 'react'
+
+export function useStateTotals(){
+    const [totals,setTotals] = useState(undefined)
+    
+    const fetchTotals = useCallback(async () => {
+        try{
+            const response = await fetch('/api/state_totals');
+            if(!response.ok){
+                throw new Error(`HTTP error, stats: ${response.status}`);
+            }
+                const result = await response.json()
+                console.log(result)
+                setTotals(result)
+        }
+        catch (error) {
+            console.error('Fetch error:',error);
+        }
+    },[]);
+
+    useEffect(() => {
+        fetchTotals();
+    },[fetchTotals]);
+
+    return {totals,refetch:fetchTotals}
+}
